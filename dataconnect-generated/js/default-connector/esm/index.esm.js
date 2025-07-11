6e1c0fa2193f6db67207e4ef44c96257f6562639
yyuu0223k <blackrock.dev011@gmail.com>
2025-07-11 21:53:26 +0900

    firebase deploy

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'pinto-ai-pro',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,7 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'pinto-ai-pro',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
